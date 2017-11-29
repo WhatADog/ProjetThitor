126ca9b1d1959ad9626e0c03c158412acefbf4fe
Victor Villemeur <victor.villemeur@gmail.com>
2017-11-29 15:50:59 +0100

    Modification du menu, ajout d'une fonction pour gérer le choix d'un user

---
+++ ProjetGS15/src/CramerShoup.java
@@ -0,0 +1,9 @@
+
+public class CramerShoup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ProjetGS15/src/VcesTrueRSA.java
+++ ProjetGS15/src/VcesTrueRSA.java
@@ -1,5 +1,29 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class VcesTrueRSA {
+	
+	// Fonction pour récupérer un entier
+	public static int recupererInt() {
+        int userValue = 0;
+        boolean erreur = false;
+        Scanner scan = new Scanner(System.in);
+        
+        //Bloc d'essai (pour s'assurer qu'on récupère bien un entier
+        do {
+            try {
+                userValue = scan.nextInt();
+                scan.next();
+                erreur = false;
+            } catch (InputMismatchException e) {
+                System.out.println("\nCe n'est pas une valeur prise en charge, veuillez réessayer.");
+                erreur = true;
+                scan.next();
+            }
+        } while (erreur);
+        scan.close();
+        return userValue;
+    }
+	
 	public static void main (String[] args){
 		Scanner user_input = new Scanner( System.in );
 		System.out.println("Selectionner votre fonction de chiffrement : \t");
@@ -9,8 +33,8 @@ public class VcesTrueRSA {
 		System.out.println("->4<- Déchiffrement symétrique ThreeFish\t");
 		System.out.println("->5<- Déchiffrement de Cramer-Shoup\t");
 		System.out.println("->6<- Vérification d'un hash\t");
-		String sChoixUser = user_input.nextLine();
-		int iChoixUser = Integer.parseInt(sChoixUser);
+		
+		int iChoixUser = recupererInt();
 		switch (iChoixUser) {
 		case 1: System.out.println("Appel de la fonction de Chiffrement symétrique ThreeFish");
 		break;
@@ -27,6 +51,8 @@ public class VcesTrueRSA {
 		default: System.out.println("Entrée invalide veuillez recommencer !");
 		break;
 		}
+		user_input.close();
 		
 	}
+	
 }
