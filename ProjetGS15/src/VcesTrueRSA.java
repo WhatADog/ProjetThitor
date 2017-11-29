8cf909a331b4030c702b93a77381fb38f2a6aaa5
Victor Villemeur <victor.villemeur@gmail.com>
2017-11-29 15:12:21 +0100

    Création du menu

---
+++ ProjetGS15/src/VcesTrueRSA.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class VcesTrueRSA {
+	public static void main (String[] args){
+		String choixUser;
+		Scanner user_input = new Scanner( System.in );
+		System.out.println("Toute la merde la : \t");
+		choixUser = user_input.nextLine();
+		System.out.println(choixUser);
+		
+	}
+}
