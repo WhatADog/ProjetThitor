61cc9f127fcaafe36137d22034514a19110b8d5b
PixelatedGrizzly <voinot.thibaut@gmail.com>
2017-12-06 23:18:36 +0100

    Ajout crible d'Erathostene et test primalité (-> CramerShoup)

--- ProjetGS15/src/CramerShoup.java
+++ ProjetGS15/src/CramerShoup.java
@@ -1,5 +1,51 @@
+import java.util.ArrayList;
 
 public class CramerShoup {
+
+	private static ArrayList<Integer> nb_premiers = new ArrayList<Integer>();
+
+	public CramerShoup(){
+
+	}
+
+	public static ArrayList<Integer> cribleErathostene(int max){
+		nb_premiers.clear();
+		for (int i = 2 ; i*i <= max ; i++) {
+			nb_premiers.add(i);
+		}
+
+		int j;
+		for (int i = 0 ; i < nb_premiers.size() ; i++) {
+			j = i+1;
+			while(j < nb_premiers.size()) {
+				if(nb_premiers.get(j)%nb_premiers.get(i)==0){
+					nb_premiers.remove(j);
+				}
+				else{
+					j++;
+				}
+			}
+		}
+
+		for (int i=0; i<nb_premiers.size(); i++) {
+			System.out.println(nb_premiers.get(i));
+		}
+
+		return nb_premiers;
+
+	}
+
+	public static boolean testPrimalite(int n){
+		ArrayList<Integer> nb_testes = cribleErathostene(n);
+
+		for (int i = 0 ; i < nb_testes.size() ; i++) {
+			if(n%nb_testes.get(i)==0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- ProjetGS15/src/VcesTrueRSA.java
+++ ProjetGS15/src/VcesTrueRSA.java
@@ -1,21 +1,21 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class VcesTrueRSA {
-	
-	// Fonction pour récupérer un entier
+
+	// Fonction pour rï¿½cupï¿½rer un entier
 	public static int recupererInt() {
         int userValue = 0;
         boolean erreur = false;
         Scanner scan = new Scanner(System.in);
-        
-        //Bloc d'essai (pour s'assurer qu'on récupère bien un entier
+
+        //Bloc d'essai (pour s'assurer qu'on rï¿½cupï¿½re bien un entier
         do {
             try {
                 userValue = scan.nextInt();
                 scan.next();
                 erreur = false;
             } catch (InputMismatchException e) {
-                System.out.println("\nCe n'est pas une valeur prise en charge, veuillez réessayer.");
+                System.out.println("\nCe n'est pas une valeur prise en charge, veuillez rï¿½essayer.");
                 erreur = true;
                 scan.next();
             }
@@ -23,36 +23,37 @@ public class VcesTrueRSA {
         scan.close();
         return userValue;
     }
-	
+
 	public static void main (String[] args){
 		Scanner user_input = new Scanner( System.in );
 		System.out.println("Selectionner votre fonction de chiffrement : \t");
-		System.out.println("->1<- Chiffrement symétrique ThreeFish\t");
+		System.out.println("->1<- Chiffrement symï¿½trique ThreeFish\t");
 		System.out.println("->2<- Chiffrement de Cramer-Shoup\t");
 		System.out.println("->3<- Hashage d'un message\t");
-		System.out.println("->4<- Déchiffrement symétrique ThreeFish\t");
-		System.out.println("->5<- Déchiffrement de Cramer-Shoup\t");
-		System.out.println("->6<- Vérification d'un hash\t");
-		
+		System.out.println("->4<- Dï¿½chiffrement symï¿½trique ThreeFish\t");
+		System.out.println("->5<- Dï¿½chiffrement de Cramer-Shoup\t");
+		System.out.println("->6<- Vï¿½rification d'un hash\t");
+
 		int iChoixUser = recupererInt();
 		switch (iChoixUser) {
-		case 1: System.out.println("Appel de la fonction de Chiffrement symétrique ThreeFish");
+		case 1: System.out.println("Appel de la fonction de Chiffrement symï¿½trique ThreeFish");
 		break;
 		case 2: System.out.println("Appel de la fonction de Chiffrement Cramer-Shoup");
+			System.out.println(CramerShoup.testPrimalite(11003));
 		break;
 		case 3: System.out.println("Appel de la fonction Hashage d'un message");
 		break;
-		case 4: System.out.println("Appel de la fonction de déchiffrement symétrique ThreeFish");
+		case 4: System.out.println("Appel de la fonction de dï¿½chiffrement symï¿½trique ThreeFish");
 		break;
-		case 5: System.out.println("Appel de la fonction de déchiffrement de Cramer-Shoup");
+		case 5: System.out.println("Appel de la fonction de dï¿½chiffrement de Cramer-Shoup");
 		break;
-		case 6: System.out.println("Appel de la fonction de vérification d'un hash");
+		case 6: System.out.println("Appel de la fonction de vï¿½rification d'un hash");
 		break;
-		default: System.out.println("Entrée invalide veuillez recommencer !");
+		default: System.out.println("Entrï¿½e invalide veuillez recommencer !");
 		break;
 		}
 		user_input.close();
-		
+
 	}
-	
+
 }
