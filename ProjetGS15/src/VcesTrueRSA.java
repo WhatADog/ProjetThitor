b219610fb06dc9d8c1e89adb6a5b6f5d7ed67c1f
Victor Villemeur <victor.villemeur@gmail.com>
2017-12-07 09:29:53 +0100

    Amelioration de recupererInt et encodage

--- ProjetGS15/src/VcesTrueRSA.java
+++ ProjetGS15/src/VcesTrueRSA.java
@@ -2,20 +2,19 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class VcesTrueRSA {
 
-	// Fonction pour rï¿½cupï¿½rer un entier
+	// Fonction pour récuperer un entier
 	public static int recupererInt() {
         int userValue = 0;
         boolean erreur = false;
         Scanner scan = new Scanner(System.in);
 
-        //Bloc d'essai (pour s'assurer qu'on rï¿½cupï¿½re bien un entier
+        //Bloc d'essai (pour s'assurer qu'on récupère bien un entier
         do {
             try {
                 userValue = scan.nextInt();
-                scan.next();
                 erreur = false;
             } catch (InputMismatchException e) {
-                System.out.println("\nCe n'est pas une valeur prise en charge, veuillez rï¿½essayer.");
+                System.out.println("\nCe n'est pas une valeur prise en charge, veuillez réessayer.");
                 erreur = true;
                 scan.next();
             }
@@ -27,16 +26,16 @@ public class VcesTrueRSA {
 	public static void main (String[] args){
 		Scanner user_input = new Scanner( System.in );
 		System.out.println("Selectionner votre fonction de chiffrement : \t");
-		System.out.println("->1<- Chiffrement symï¿½trique ThreeFish\t");
+		System.out.println("->1<- Chiffrement symétrique ThreeFish\t");
 		System.out.println("->2<- Chiffrement de Cramer-Shoup\t");
 		System.out.println("->3<- Hashage d'un message\t");
-		System.out.println("->4<- Dï¿½chiffrement symï¿½trique ThreeFish\t");
-		System.out.println("->5<- Dï¿½chiffrement de Cramer-Shoup\t");
-		System.out.println("->6<- Vï¿½rification d'un hash\t");
+		System.out.println("->4<- Déchiffrement symétrique ThreeFish\t");
+		System.out.println("->5<- Déchiffrement de Cramer-Shoup\t");
+		System.out.println("->6<- Vérification d'un hash\t");
 
 		int iChoixUser = recupererInt();
 		switch (iChoixUser) {
-		case 1: System.out.println("Appel de la fonction de Chiffrement symï¿½trique ThreeFish");
+		case 1: System.out.println("Appel de la fonction de Chiffrement symétrique ThreeFish");
 		break;
 		case 2: System.out.println("Appel de la fonction de Chiffrement Cramer-Shoup");
 			System.out.println(7+" : "+CramerShoup.testPrimalite(7));
@@ -46,13 +45,13 @@ public class VcesTrueRSA {
 		break;
 		case 3: System.out.println("Appel de la fonction Hashage d'un message");
 		break;
-		case 4: System.out.println("Appel de la fonction de dï¿½chiffrement symï¿½trique ThreeFish");
+		case 4: System.out.println("Appel de la fonction de déchiffrement symétrique ThreeFish");
 		break;
-		case 5: System.out.println("Appel de la fonction de dï¿½chiffrement de Cramer-Shoup");
+		case 5: System.out.println("Appel de la fonction de déchiffrement de Cramer-Shoup");
 		break;
-		case 6: System.out.println("Appel de la fonction de vï¿½rification d'un hash");
+		case 6: System.out.println("Appel de la fonction de vérification d'un hash");
 		break;
-		default: System.out.println("Entrï¿½e invalide veuillez recommencer !");
+		default: System.out.println("Entrée invalide veuillez recommencer !");
 		break;
 		}
 		user_input.close();
