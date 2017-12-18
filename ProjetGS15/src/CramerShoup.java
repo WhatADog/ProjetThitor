039dc3b630e3943689a3d8266933ee53a4b96649
PixelatedGrizzly <voinot.thibaut@gmail.com>
2017-12-18 17:35:28 +0100

    Fonction de génération de premier non friable et d'élément générateur

--- ProjetGS15/src/CramerShoup.java
+++ ProjetGS15/src/CramerShoup.java
@@ -1,85 +1,63 @@
+
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Random;
 
 public class CramerShoup {
 
-	private static ArrayList<Integer> nb_premiers = new ArrayList<Integer>();
+	private static ArrayList<Long> nb_premiers = new ArrayList<Long>();
 
 	public CramerShoup(){
 
 	}
 
-	public static ArrayList<Integer> cribleErathostene(int max){
-		nb_premiers.clear();
-		for (int i = 2 ; i*i <= max ; i++) {
-			nb_premiers.add(i);
-		}
+	public static ArrayList<Long> genElementGenerateurSurNonFriable(BigInteger bi){
+		BigInteger ordreRecherche = new BigInteger(bi.toByteArray());
+		ordreRecherche = ordreRecherche.subtract(BigInteger.valueOf(1));
+		BigInteger secondFacteur = new BigInteger(ordreRecherche.toByteArray());
+		secondFacteur = secondFacteur.divide(BigInteger.valueOf(2));
 
-		int j;
-		for (int i = 0 ; i < nb_premiers.size() ; i++) {
-			j = i+1;
-			while(j < nb_premiers.size()) {
-				if(nb_premiers.get(j)%nb_premiers.get(i)==0){
-					nb_premiers.remove(j);
-				}
-				else{
-					j++;
-				}
-			}
-		}
-		/*
-		for (int i=0; i<nb_premiers.size(); i++) {
-			System.out.println(nb_premiers.get(i));
-		}
-		*/
-		return nb_premiers;
+		BigInteger test;
+		ArrayList<Long> result = new ArrayList<Long>();
 
-	}
-
-	public static boolean testPrimalite(int n){
-		ArrayList<Integer> nb_testes = cribleErathostene(n);
-
-		for (int i = 0 ; i < nb_testes.size() ; i++) {
-			if(n%nb_testes.get(i)==0){
-				return false;
+		for (long i = 2 ; i <= 101 ; i++) {
+			test = BigInteger.valueOf(i);
+			if(test.modPow(secondFacteur, bi).compareTo(BigInteger.ONE) != 0 && test.modPow(BigInteger.valueOf(2), bi).compareTo(BigInteger.ONE) != 0){
+				result.add(i);
 			}
 		}
 
-		return true;
-	}
-
-	public static boolean verifElemGenerateur(int n, int a){
-
-		if (a==0) {
-			return false;
-		}
-
-		int p = n-1;
-		ArrayList<Integer> nb_composants = cribleErathostene(p);
-		int limit = p / getPlusPetitFacteurPremier(p, nb_composants);
-
-		int a_Generated = a;
-		for (int i=1; i<=limit ; i++) {
-			System.out.println(a_Generated);
-			a_Generated = (a_Generated*a)%n;
-			if(a_Generated==1){
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public static int getPlusPetitFacteurPremier(int p, ArrayList<Integer> facteursPremiers){
-		for (int i =0; i<facteursPremiers.size() ; i++) {
-			if (p%facteursPremiers.get(i)==0) {
-				return facteursPremiers.get(i);
-			}
-		}
-		return 0;
+		return result;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random rnd = new Random();
 
+		BigInteger test = new BigInteger(256, 100, rnd);
+		BigInteger deriv = new BigInteger(test.toByteArray());
+		deriv = deriv.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2));
+		boolean friable = true;
+
+		if (deriv.isProbablePrime(100)) {
+			friable = false;
+		}
+		else{
+			while(friable){
+				test = new BigInteger(256, 100, rnd);
+				deriv = new BigInteger(test.toByteArray());
+				deriv = deriv.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2));
+				if(deriv.isProbablePrime(100)){
+					friable = false;
+				}
+			}
+		}
+
+		System.out.println(test.toString());
+		System.out.println(deriv.toString());
+
+		ArrayList<Long> premElemGenerateur = genElementGenerateurSurNonFriable(test);
+		System.out.println(premElemGenerateur.get(premElemGenerateur.size()-1)+" ; "+premElemGenerateur.get(premElemGenerateur.size()-2));
 	}
 
 }
